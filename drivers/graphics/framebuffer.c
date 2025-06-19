9c109fb6aea94387894d68aa95a3fa62b4a7b4e7
ayjays132 <Ayjays.ph@gmail.com>
2025-06-19 02:04:30 -0700

    Add driver skeletons

---
+++ drivers/graphics/framebuffer.c
@@ -0,0 +1,8 @@
+#include "framebuffer.h"
+
+// Placeholder for generic framebuffer initialization
+void init_framebuffer(void) {
+    // TODO: initialize framebuffer using bootloader-provided mode info
+}
+
+// TODO: Add Nvidia and AMD GPU driver support in the future
---
+++ drivers/graphics/framebuffer.h
@@ -0,0 +1,6 @@
+#ifndef PHILLOS_FRAMEBUFFER_H
+#define PHILLOS_FRAMEBUFFER_H
+
+void init_framebuffer(void);
+
+#endif // PHILLOS_FRAMEBUFFER_H
