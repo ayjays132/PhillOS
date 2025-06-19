26e626233ce20ef71b9c28ef5a9c3bafd44a98a4
ayjays132 <Ayjays.ph@gmail.com>
2025-06-19 05:07:22 -0700

    docs: add build instructions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Requires the x86_64-elf cross toolchain and EFI utilities in PATH
+# See docs/building.md for setup instructions.
+
 # Build bootloader and kernel
 make -C bootloader OUT_DIR=../dist/bootloader
 
