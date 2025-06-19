293696db1257e0ae5ee5e7761c0f10aa4c83abec
ayjays132 <Ayjays.ph@gmail.com>
2025-06-19 02:24:27 -0700

    Implement basic AHCI driver

--- drivers/storage/ahci.h
+++ drivers/storage/ahci.h
@@ -1,6 +1,10 @@
 #ifndef PHILLOS_AHCI_H
 #define PHILLOS_AHCI_H
 
+#include <stdint.h>
+
 void init_ahci(void);
+int ahci_read(uint64_t lba, uint32_t count, void *buffer);
+int ahci_write(uint64_t lba, uint32_t count, const void *buffer);
 
 #endif // PHILLOS_AHCI_H
