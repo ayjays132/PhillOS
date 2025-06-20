d1fafc4174e67e372b06c06c2e8bac1b1d1dd4dd
ayjays132 <Ayjays.ph@gmail.com>
2025-06-20 07:43:16 -0700

    build: compile scheduler

--- kernel/init.h
+++ kernel/init.h
@@ -6,4 +6,8 @@
 void kernel_main(boot_info_t *boot_info);
 boot_info_t *boot_info_get(void);
 
+int schedule_resources(const float *A, const float *B,
+                       const float *R_tot, size_t N, size_t M, size_t R,
+                       float *out_x);
+
 #endif // PHILLOS_INIT_H
---
+++ kernel/scheduler/uhs.h
@@ -0,0 +1,10 @@
+#ifndef PHILLOS_UHS_H
+#define PHILLOS_UHS_H
+
+#include <stddef.h>
+
+int uhs_compute(const float *A, const float *B,
+                const float *R_tot, size_t N, size_t M, size_t R,
+                float *out_x);
+
+#endif // PHILLOS_UHS_H
